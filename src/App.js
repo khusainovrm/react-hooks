7247f5a7518766f776a67d822d1aede74678216f
Rinat Khusainov <khusainovrm@yandex.ru>
2020-06-19 11:03:25 +0300

    useState

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import TodoList from './TodoList'
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'First todo', completed: false },
+    { id: 2, title: 'Second todo', completed: false },
+  ])
+
+  const [todoTitle, setTodoTitle] = useState('')
+
+  const addTodo = (event) => {
+    if (event.key === 'Enter') {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: todoTitle,
+          completed: false,
+        },
+      ])
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1>Todo app</h1>
+
+      <div className="input-field">
+        <input
+          type="text"
+          value={todoTitle}
+          onChange={(event) => setTodoTitle(event.target.value)}
+          onKeyPress={addTodo}
+        />
+        <label>Todo name</label>
+      </div>
+
+      <TodoList todos={todos} />
+    </div>
+  )
+}
